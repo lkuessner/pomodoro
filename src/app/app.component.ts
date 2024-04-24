6aa17577a9aafadb8354e163f273f1519e9d8f20
lkuessner <leonkuessner@outlook.com>
2024-04-25 00:18:59 +0200

    chore: provide stores in app config, add elements to test services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,128 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { ConfigActions } from './state/config/config.actions';
 import { ConfigService } from './services/ConfigService/config.service';
+import { LogService } from './services/LogService/log.service';
+import { CommonModule } from '@angular/common';
+import { TimerService } from './services/TimerService/timer.service';
+import { Observable } from 'rxjs';
+import { Timer } from './interfaces/timer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'pomodoro';
-  inputValue: number = 25;
-  constructor(private configService: ConfigService) {}
+  taskInputValue: number | undefined;
+  breakInputValue: number | undefined;
+  logId: string = '';
+  timers$: Observable<Array<Timer>> | undefined;
+  firstTimerId: string = '';
+  firstTaskId: string = '';
+  constructor(
+    private configService: ConfigService,
+    private logService: LogService,
+    private timerService: TimerService
+  ) {}
 
   ngOnInit(): void {
-    console.log('app component onInit', this.configService.getConfig());
+    this.configService.getConfigState().subscribe((state) => {
+      this.taskInputValue = state.taskDuration;
+      this.breakInputValue = state.breakDuration;
+    });
+    this.timers$ = this.timerService.getAllTimers();
+    this.timers$.subscribe((timer) => {
+      this.firstTimerId = timer[0].id;
+      this.firstTaskId = timer[0]?.tasks[0]?.id;
+    });
   }
 
+  /** TimerService Methods */
+  addTimer() {
+    return this.timerService.addTimer();
+  }
+
+  toggleTimerIsActive() {
+    return this.timerService.toggleTimerIsActive(this.firstTimerId);
+  }
+
+  toggleTimerIsExpired() {
+    return this.timerService.toggleTimerIsExpired(this.firstTimerId);
+  }
+
+  addTask() {
+    return this.timerService.addTask(this.firstTimerId, 'Beispiel Task Titel');
+  }
+
+  removeTask() {
+    return this.timerService.removeTask(this.firstTimerId, this.firstTaskId);
+  }
+
+  setTaskTitle() {
+    return this.timerService.setTaskTitle(
+      this.firstTimerId,
+      this.firstTaskId,
+      'Changed Task title'
+    );
+  }
+
+  setTaskDone() {
+    return this.timerService.toggleTaskIsDone(
+      this.firstTimerId,
+      this.firstTaskId
+    );
+  }
+
+  /** LogService Methods */
+  getLogState() {
+    return this.logService.getLogState();
+  }
+
+  resetLogState() {
+    this.logService.resetLogState();
+  }
+  removeLogById() {
+    this.logService.removeLogById(this.logId);
+  }
+
+  /** ConfigService Methods */
   getConfig() {
-    this.configService
-      .getConfig()
-      .subscribe((data) => console.log('getConfig', data));
+    return this.configService.getConfigState();
   }
 
   getTaskDuration() {
-    this.configService
-      .getTaskDuration()
-      .subscribe((data) => console.log('getTaskDuration', data));
+    return this.configService.getTaskDuration();
   }
 
   getBreakDuration() {
-    this.configService
-      .getBreakDuration()
-      .subscribe((data) => console.log('getBreakDuration', data));
+    return this.configService.getBreakDuration();
+  }
+
+  setTaskDuration() {
+    if (this.taskInputValue)
+      this.configService.setTaskDuration(this.taskInputValue);
   }
 
   setBreakDuration() {
-    this.configService.setTaskDuration(this.inputValue);
+    if (this.breakInputValue)
+      this.configService.setBreakDuration(this.breakInputValue);
   }
 
-  displayInputValue() {
-    console.log(this.inputValue);
-  }
-
-  handleInputChange(event: Event) {
+  handleTaskDurationChange(event: Event) {
     const newValue = Number((event.target as HTMLInputElement).value);
-    this.inputValue = newValue;
+    this.taskInputValue = newValue;
+  }
+
+  handleBreakDurationChange(event: Event) {
+    const newValue = Number((event.target as HTMLInputElement).value);
+    this.breakInputValue = newValue;
+  }
+
+  handleLogIdInputChange(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.logId = inputValue;
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { configReducer } from './state/config/config.reducer';
+import { logReducer } from './state/log/log.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { LogEffects } from './state/log/log.effects';
+import { timerReducer } from './state/timer/timer.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore({ config: configReducer })],
+  providers: [
+    provideRouter(routes),
+    provideStore({
+      config: configReducer,
+      logs: logReducer,
+      timers: timerReducer,
+    }),
+    provideEffects(LogEffects),
+  ],
 };
